08cc2551eda82699ea95a83f7c4d75c40ca04346
Romina <rgrasso@renovofinancial.com>
2026-09-18 16:59:52 -0300

    da

--- Home/packages-builder-pricing.js
+++ Home/packages-builder-pricing.js
@@ -140,6 +140,7 @@
   /** Single-rate activities from home-card prices (per person unless overridden). */
   const SINGLE_PER_PERSON = [
     ["cabal.html", 40],
+    ["colonia-wellness.html", 85],
     ["kayak.html", 80],
     ["candombe.html", 80],
     ["mate.html", 40],
